07e9b581d55b1d6aa8b5f5932cc3e6106847cb8a
Junaid <junaidofficialnow@gmail.com>
2024-04-18 17:21:16 +0530

    feat: created initial component designs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+  <div class="header">
+    <h1>Create your  certificate</h1>
+     <h3>Select a template, enter details and generate!</h3>
+  </div>
 
-    <router-outlet />
+  <div class="templates">
+    <img src="./assets/sample.jpg" alt="" class="template">
+    <img src="./assets/sample.jpg" alt="" class="template">
+    <img src="./assets/sample.jpg" alt="" class="template">
+    <img src="./assets/sample.jpg" alt="" class="template">
+  </div>
   `,
-  styles: [],
+  styles: `
+  .templates {
+    display:  flex;
+    gap: 2rem; 
+    justify-content: center;
+    margin-top: 2rem;
+  }
+  .template {
+     width: 13rem;
+     height: 9rem;
+  }
+    h1 {
+      font-weight: 700;
+      font-size: 44px;
+      margin-bottom: 0;
+
+    }
+    .header {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  `
 })
-export class AppComponent {
-  title = 'certificate-generator';
-}
+export class AppComponent {}
